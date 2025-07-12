37a973248ff65c53419a37ac260c7c3b019499ee
Saloni Jaiswal <salonijaiswal622@gmail.com>
2025-07-12 14:10:43 +0530

    fixed type issue

--- src/types/status.ts
+++ src/types/status.ts
@@ -1,7 +1,9 @@
-export enum STATUS {
-    IDLE = 'idle',
-    LOADING = 'loading',
-    SUCCEEDED = 'succeeded',
-    FAILED = 'failed'
-}
+export const STATUS = {
+    IDLE: 'idle',
+    LOADING: 'loading',
+    SUCCEEDED: 'succeeded',
+    FAILED: 'failed'
+} as const;
+
+export type STATUS = typeof STATUS[keyof typeof STATUS];
   
