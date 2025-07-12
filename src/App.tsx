28f757d8c5f053421e71e94349d9a1cc3eb5521f
Saloni Jaiswal <salonijaiswal622@gmail.com>
2025-07-12 13:04:05 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ProductListingPage from './pages/productListingPage/ProductListingPage'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <ProductListingPage/>
+    </div>
+  )
+}
+
+export default App
